da0d81f45d8fa2eabcc2b917cf4b592ec114d419
naveensaravanan <jayabalannaveen@gmail.com>
2019-01-02 18:11:32 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalComponent } from './global/global.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalComponent
   ],
   imports: [
     BrowserModule,
